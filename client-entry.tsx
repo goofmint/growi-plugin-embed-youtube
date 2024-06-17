f006c83ae07a8905972a4327c37cc4e4e80138a1
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2024-06-17 21:44:13 +0900

    init

---
+++ client-entry.tsx
@@ -0,0 +1,37 @@
+import { EmbedYouTube } from './src/EmbedYouTube';
+import { Options, Func, ViewOptions } from './types/utils';
+
+declare const growiFacade : {
+  markdownRenderer?: {
+    optionsGenerators: {
+      customGenerateViewOptions: (path: string, options: Options, toc: Func) => ViewOptions,
+      generateViewOptions: (path: string, options: Options, toc: Func) => ViewOptions,
+    },
+  },
+};
+
+const activate = (): void => {
+  if (growiFacade == null || growiFacade.markdownRenderer == null) {
+    return;
+  }
+  const { optionsGenerators } = growiFacade.markdownRenderer;
+  optionsGenerators.customGenerateViewOptions = (...args) => {
+    const options = optionsGenerators.generateViewOptions(...args);
+    const A = options.components.a;
+    // replace
+    options.components.a = EmbedYouTube(A);
+    return options;
+  };
+};
+
+const deactivate = (): void => {
+};
+
+// register activate
+if ((window as any).pluginActivators == null) {
+  (window as any).pluginActivators = {};
+}
+(window as any).pluginActivators['growi-plugin-copy-code-to-clipboard2'] = {
+  activate,
+  deactivate,
+};
---
+++ src/EmbedYouTube.tsx
@@ -0,0 +1,36 @@
+import innerText from 'react-innertext';
+
+import './EmbedYouTube.css';
+
+const getYouTubeId = (href: string): string | null => {
+  const url = new URL(href);
+  const videoId = url.searchParams.get('v');
+  if (videoId) return videoId;
+  return null;
+};
+
+export const EmbedYouTube = (A: React.FunctionComponent<any>): React.FunctionComponent<any> => {
+  return ({ children, href, ...props }) => {
+    const videoId = getYouTubeId(href);
+    if (!videoId) {
+      return (
+        <>
+          <A {...props}>{children}</A>
+        </>
+      );
+    }
+    return (
+      <div className="youtube">
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+    );
+  };
+};
